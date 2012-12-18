442a477d03b14fe4e0c84c1eae4b14edf43b0745
Bryson D. Hair <bdhair198@aol.com>
2012-12-18 16:26:18 -0500

    Successfully upload a file - and throw away the data.

--- fuel/app/classes/controller/files.php
+++ fuel/app/classes/controller/files.php
@@ -10,7 +10,18 @@ class Controller_Files extends Controller
 
 	public function action_create()
 	{
-		return Response::forge(View::forge('files/create'));
+		$data['refresh_interval'] = null;
+
+		if(Input::method() == 'POST')
+		{
+			Upload::process();
+			if(Upload::get_files('content'))
+			{
+				$data['alerts'] = array(array('success', 'File uploaded successfully.'));
+			}
+		}
+
+		return Response::forge(View::forge('files/create', $data));
 	}
 }
 
--- fuel/app/views/files/create.php
+++ fuel/app/views/files/create.php
@@ -3,15 +3,47 @@
 <head>
 	<meta charset="utf-8">
 	<title>Hello, <?php echo 'Atitle'; ?></title>
-	<?php echo Asset::css('bootstrap.css'); ?>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css" rel="stylesheet">
+	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/themes/ui-lightness/jquery-ui.css" type="text/css" media="all" />
+	<script src="/assets/js/datatables.js"></script>
+	<link href="/assets/css/datatables.css" rel="stylesheet">
+	<style>
+		body { margin: 40px; }
+	</style>
+	<script>
+		$(document).ready(function(){
+			$('.dropdown-toggle').dropdown();
+			$('.alert').alert();
+		});
+	</script>
+	<?php if(isset($refresh_interval)): ?>
+		<meta http-equiv="refresh" content="<?php echo $refresh_interval; ?>;/index/files/create" />
+	<?php endif; ?>
 </head>
 <body>
-	<div id="header">
-		<div class="row">
-			<div id="logo"></div>
-		</div>
-	</div>
 	<div class="container">
+		<?php if(isset($alerts)): ?>
+			<div id='flash-messages'>
+				<?php foreach($alerts as $alert): ?>
+					<div class='alert alert-<?php echo $alert[0]; ?>'>
+						<a class='close' href='#' data-dismiss='alert'>&times;</a>
+						<?php echo $alert[1]; ?>
+					</div>
+				<?php endforeach; ?>
+			</div>
+		<?php endif; ?>
+		<h2>Upload a File</h2>
+		<form method='post' class='form form-horizontal' enctype='multipart/form-data'>
+			<div class='form-inputs'>
+				<input type='file' name='content' />
+			</div>
+			<div class='form-actions'>
+				<button type='submit' class='btn btn-danger'>Upload File</button>
+			</div>
+		</form>
 	</div>
 </body>
 </html>
